3a598880b3908a563667dc66c07073652e641a3f
xinxin5080 <xinxin5080@163.com>
2019-01-11 17:26:21 +0800

    wjs

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+  whatScreen();
+  
+    $('[data-toggle="tooltip"]').tooltip()
+ 
+  // 浏览器的大小改变之后 
+  onresize = function () {
+    whatScreen();
+  }
+
+  function whatScreen() {
+    var width = document.querySelector("html").offsetWidth;
+    var title = document.querySelector("title");
+    if (width <= 768) {
+      title.innerHTML = "极小屏幕-" + width;
+    } else if (width > 768 && width <= 992) {
+      title.innerHTML = "小屏幕-" + width;
+    } else if (width > 992 && width <= 1200) {
+      title.innerHTML = "普通屏幕-" + width;
+    } else if (width > 1200) {
+      title.innerHTML = "大屏幕-" + width;
+    }
+  }
+
+})
